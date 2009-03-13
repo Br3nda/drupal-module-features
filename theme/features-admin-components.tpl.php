3744bc1fb8bde53bafdc59ff37ce8a9a1838d3c6
yhahn <yhahn>
2009-03-13 21:59:23 +0000

    Adding first commit of features module

---
+++ theme/features-admin-components.tpl.php
@@ -0,0 +1,14 @@
+<div class='clear-block features-components'>
+  <div class='column'>
+    <div class='info'>
+      <h3><?php print $info['name'] ?></h3>
+      <div class='description'><?php print $info['description'] ?></div>
+      <?php print $dependencies ?>
+    </div>
+  </div>
+  <div class='column'>
+    <div class='components'>
+      <?php print $components ?>
+    </div>
+  </div>
+</div>
