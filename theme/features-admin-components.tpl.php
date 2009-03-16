ef55aedf74ba0312b9b1350fcea0c1f832fbb559
jmiccolis <jmiccolis>
2009-03-16 15:18:58 +0000

    Adding a set of missing Id tags.

--- theme/features-admin-components.tpl.php
+++ theme/features-admin-components.tpl.php
@@ -1,3 +1,6 @@
+<?php
+// $Id: features-admin-components.tpl.php,v 1.1.2.1 2009/03/16 15:18:58 jmiccolis Exp $
+?>
 <div class='clear-block features-components'>
   <div class='column'>
     <div class='info'>
