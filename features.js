189e809f956456a14bffd100742c0d401559c56b
yhahn <yhahn>
2009-03-17 13:08:55 +0000

    #404206: Adding handling for custom code components.

--- features.js
+++ features.js
@@ -1,4 +1,4 @@
-// $Id: features.js,v 1.1.2.2 2009/03/16 23:26:54 yhahn Exp $
+// $Id: features.js,v 1.1.2.3 2009/03/17 13:08:55 yhahn Exp $
 
 Drupal.behaviors.featuresTable = function() {
   $('table.features input').bind('change', function() {
@@ -13,23 +13,23 @@ Drupal.behaviors.featuresTable = function() {
 
 Drupal.behaviors.featuresMachineReadable = function() {
   if ($('.feature-name').size() > 0) {
-    $('.feature-name').after(' <small class="feature-filename-suffix">&nbsp;</small>');
-    if ($('.feature-filename').val() == $('.feature-name').val().toLowerCase().replace(/[^a-z0-9]+/g, '_').replace(/_+/g, '_') || $('.feature-filename').val() == '') {
-      $('.feature-filename').parents('.form-item').hide();
+    $('.feature-name').after(' <small class="feature-module-name-suffix">&nbsp;</small>');
+    if ($('.feature-module-name').val() == $('.feature-name').val().toLowerCase().replace(/[^a-z0-9]+/g, '_').replace(/_+/g, '_') || $('.feature-module-name').val() == '') {
+      $('.feature-module-name').parents('.form-item').hide();
       $('.feature-name').keyup(function() {
         var machine = $(this).val().toLowerCase().replace(/[^a-z0-9]+/g, '_').replace(/_+/g, '_');
         if (machine != '_' && machine != '') {
-          $('.feature-filename').val(machine);
-          $('.feature-filename-suffix').empty().append(' Machine name: ' + machine + ' [').append($('<a href="#">'+ Drupal.t('Edit') +'</a>').click(function() {
-            $('.feature-filename').parents('.form-item').show();
-            $('.feature-filename-suffix').hide();
+          $('.feature-module-name').val(machine);
+          $('.feature-module-name-suffix').empty().append(' Machine name: ' + machine + ' [').append($('<a href="#">'+ Drupal.t('Edit') +'</a>').click(function() {
+            $('.feature-module-name').parents('.form-item').show();
+            $('.feature-module-name-suffix').hide();
             $('.feature-name').unbind('keyup');
             return false;
           })).append(']');
         }
         else {
-          $('.feature-filename').val(machine);
-          $('.feature-filename-suffix').text('');
+          $('.feature-module-name').val(machine);
+          $('.feature-module-name-suffix').text('');
         }
       });
       $('.feature-name').keyup();
